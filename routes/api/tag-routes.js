4b6c464653c029378e2986e90685434fa7f1b607
Albert <alberto.suarez8@outlook.com>
2023-01-06 23:47:04 -0500

    Added post, put and delete routes to tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -31,16 +31,46 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagsData = await Tag.create({
+      tag_name: req.body.tag_name,
+    });
+    res.status(200).json(tagsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagsData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!tagsData) {
+      res.status(404).json({ message: 'No tag found with that id.' });
+    }
+    tagsData.update({ tag_name: req.body.tag_name })
+    res.status(200).json(tagsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagsData = await Tag.destroy({
+      where: {
+      id: req.params.id,
+      },
+    });
+    res.status(200).json(tagsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
